e2af020a56e606571fd25343681f3949d92c0bea
Noah Everett <noaheverett898@gmail.com>
2024-08-02 14:06:40 -0500

    Fix THistReader file close

---
+++ DataModel/THistReader.cpp
@@ -0,0 +1,459 @@
+//*/////////////////////////////////////////////////////////////////////////////////////////////*//
+//*//                                       THistReader.cpp                                   //*//
+//*/////////////////////////////////////////////////////////////////////////////////////////////*//
+//*//                                                                                         //*//
+//*// Author:                                                                                 //*//
+//*//   Noah Everett (noah.everett@mines.sdsmt.edu)                                           //*//
+//*//   Created ¯\_(ツ)_/¯                                                                    //*//
+//*//                                                                                         //*//
+//*// About:                                                                                  //*//
+//*//   This file contains a general class for reading ROOT histograms.                       //*//
+//*//                                                                                         //*//
+//*// Notation:                                                                               //*//
+//*//   t_ = function argument                                                                //*//
+//*//   m_ = member variable                                                                  //*//
+//*//   mainVariable_subCategory (use '_' like LaTeX)                                         //*//
+//*//                                                                                         //*//
+//*/////////////////////////////////////////////////////////////////////////////////////////////*//
+//*//                                                                                         //*//
+//*/////////////////////////////////////////////////////////////////////////////////////////////*//
+
+#include "THistReader.h"
+
+TH1D* copy_TH( const TH1D* t_original ) {
+    if( ! t_original ) {
+        cout << "Error: Original histogram is null" << endl;
+        return nullptr;
+    }
+
+    vector< void* > exported = export_TH( t_original );
+    if( exported.empty() ) {
+        cout << "Error: Could not export histogram" << endl;
+        return nullptr;
+    }
+
+    TH1D* t_copy = nullptr;
+    import_TH( exported, t_copy );
+    if( ! t_copy ) {
+        cout << "Error: Could not copy histogram" << endl;
+        return nullptr;
+    }
+
+    return t_copy;
+}
+
+TH2D* copy_TH( const TH2D* t_original ) {
+    if( ! t_original ) {
+        cout << "Error: Original histogram is null" << endl;
+        return nullptr;
+    }
+
+    vector< void* > exported = export_TH( t_original );
+    if( exported.empty() ) {
+        cout << "Error: Could not export histogram" << endl;
+        return nullptr;
+    }
+
+    TH2D* t_copy = nullptr;
+    import_TH( exported, t_copy );
+    if( ! t_copy ) {
+        cout << "Error: Could not copy histogram" << endl;
+        return nullptr;
+    }
+
+    return t_copy;
+}
+
+TH3D* copy_TH( const TH3D* t_original ) {
+    if( ! t_original ) {
+        cout << "Error: Original histogram is null" << endl;
+        return nullptr;
+    }
+
+    vector< void* > exported = export_TH( t_original );
+    if( exported.empty() ) {
+        cout << "Error: Could not export histogram" << endl;
+        return nullptr;
+    }
+
+    TH3D* t_copy = nullptr;
+    import_TH( exported, t_copy );
+    if( ! t_copy ) {
+        cout << "Error: Could not copy histogram" << endl;
+        return nullptr;
+    }
+
+    return t_copy;
+}
+
+vector< void* > export_TH( const TH1D* t_original ) {
+    if( ! t_original ) {
+        cout << "Error: Original histogram is null" << endl;
+        return {};
+    }
+
+    vector< void* > exported;
+    exported.push_back( ( void* )( new TString( t_original->GetName() ) ) );
+    exported.push_back( ( void* )( new TString( t_original->GetTitle() ) ) );
+    exported.push_back( ( void* )( new Int_t( t_original->GetNbinsX() ) ) );
+    exported.push_back( ( void* )( new Double_t( t_original->GetXaxis()->GetXmin() ) ) );
+    exported.push_back( ( void* )( new Double_t( t_original->GetXaxis()->GetXmax() ) ) );
+
+    for( Int_t i = 1; i <= t_original->GetNbinsX(); ++i ) {
+        exported.push_back( ( void* )( new Double_t( t_original->GetBinContent( i ) ) ) );
+        exported.push_back( ( void* )( new Double_t( t_original->GetBinError( i ) ) ) );
+    }
+
+    return exported;
+}
+
+TH1D* import_TH( vector< void* >& t_exported ) {
+    if( t_exported.empty() ) {
+        cout << "Error: Exported histogram is empty" << endl;
+        return nullptr;
+    }
+
+    TString*  name   = static_cast< TString* >( t_exported[ 0 ] );
+    TString*  title  = static_cast< TString* >( t_exported[ 1 ] );
+    Int_t*    nxbins = static_cast< Int_t* >( t_exported[ 2 ] );
+    Double_t* xlow   = static_cast< Double_t* >( t_exported[ 3 ] );
+    Double_t* xhigh  = static_cast< Double_t* >( t_exported[ 4 ] );
+
+    TH1D* t_copy = new TH1D( *name, *title, *nxbins, *xlow, *xhigh );
+
+    delete name;
+    delete title;
+    delete nxbins;
+    delete xlow;
+    delete xhigh;
+
+    for( Int_t i = 1; i <= nxbins; ++i ) {
+        Double_t* content = static_cast< Double_t* >( t_exported[ 5 + 2 * ( i - 1 ) ] );
+        Double_t* error   = static_cast< Double_t* >( t_exported[ 6 + 2 * ( i - 1 ) ] );
+
+        t_copy->SetBinContent( i, *content );
+        t_copy->SetBinError( i, *error );
+
+        delete content;
+        delete error;
+    }
+
+    return t_copy;
+}
+
+vector< void* > export_TH( const TH2D* t_original ) {
+    if( ! t_original ) {
+        cout << "Error: Original histogram is null" << endl;
+        return {};
+    }
+
+    vector< void* > exported;
+    exported.push_back( ( void* )( new TString( t_original->GetName() ) ) );
+    exported.push_back( ( void* )( new TString( t_original->GetTitle() ) ) );
+    exported.push_back( ( void* )( new Int_t( t_original->GetNbinsX() ) ) );
+    exported.push_back( ( void* )( new Double_t( t_original->GetXaxis()->GetXmin() ) ) );
+    exported.push_back( ( void* )( new Double_t( t_original->GetXaxis()->GetXmax() ) ) );
+    exported.push_back( ( void* )( new Int_t( t_original->GetNbinsY() ) ) );
+    exported.push_back( ( void* )( new Double_t( t_original->GetYaxis()->GetXmin() ) ) );
+    exported.push_back( ( void* )( new Double_t( t_original->GetYaxis()->GetXmax() ) ) );
+
+    for( Int_t i = 1; i <= t_original->GetNbinsX(); ++i ) {
+        for( Int_t j = 1; j <= t_original->GetNbinsY(); ++j ) {
+            exported.push_back( ( void* )( new Double_t( t_original->GetBinContent( i, j ) ) ) );
+            exported.push_back( ( void* )( new Double_t( t_original->GetBinError( i, j ) ) ) );
+        }
+    }
+
+    return exported;
+}
+
+TH2D* import_TH( vector< void* >& t_exported ) {
+    if( t_exported.empty() ) {
+        cout << "Error: Exported histogram is empty" << endl;
+        return nullptr;
+    }
+
+    TString*  name   = static_cast< TString* >( t_exported[ 0 ] );
+    TString*  title  = static_cast< TString* >( t_exported[ 1 ] );
+    Int_t*    nxbins = static_cast< Int_t* >( t_exported[ 2 ] );
+    Double_t* xlow   = static_cast< Double_t* >( t_exported[ 3 ] );
+    Double_t* xhigh  = static_cast< Double_t* >( t_exported[ 4 ] );
+    Int_t*    nybins = static_cast< Int_t* >( t_exported[ 5 ] );
+    Double_t* ylow   = static_cast< Double_t* >( t_exported[ 6 ] );
+    Double_t* yhigh  = static_cast< Double_t* >( t_exported[ 7 ] );
+
+    TH2D* t_copy = new TH2D( *name, *title, *nxbins, *xlow, *xhigh, *nybins, *ylow, *yhigh );
+
+    delete name;
+    delete title;
+    delete nxbins;
+    delete xlow;
+    delete xhigh;
+    delete nybins;
+    delete ylow;
+    delete yhigh;
+
+    for( Int_t i = 1; i <= nxbins; ++i ) {
+        for( Int_t j = 1; j <= nybins; ++j ) {
+            Double_t* content = static_cast< Double_t* >( t_exported[ 8 + 2 * ( i - 1 ) * nybins + 2 * ( j - 1 ) ] );
+            Double_t* error   = static_cast< Double_t* >( t_exported[ 9 + 2 * ( i - 1 ) * nybins + 2 * ( j - 1 ) ] );
+
+            t_copy->SetBinContent( i, j, *content );
+            t_copy->SetBinError( i, j, *error );
+
+            delete content;
+            delete error;
+        }
+    }
+
+    return t_copy;
+}
+
+vector< void* > export_TH( const TH3D* t_original ) {
+    if( ! t_original ) {
+        cout << "Error: Original histogram is null" << endl;
+        return {};
+    }
+
+    vector< void* > exported;
+    exported.push_back( ( void* )( new TString( t_original->GetName() ) ) );
+    exported.push_back( ( void* )( new TString( t_original->GetTitle() ) ) );
+    exported.push_back( ( void* )( new Int_t( t_original->GetNbinsX() ) ) );
+    exported.push_back( ( void* )( new Double_t( t_original->GetXaxis()->GetXmin() ) ) );
+    exported.push_back( ( void* )( new Double_t( t_original->GetXaxis()->GetXmax() ) ) );
+    exported.push_back( ( void* )( new Int_t( t_original->GetNbinsY() ) ) );
+    exported.push_back( ( void* )( new Double_t( t_original->GetYaxis()->GetXmin() ) ) );
+    exported.push_back( ( void* )( new Double_t( t_original->GetYaxis()->GetXmax() ) ) );
+    exported.push_back( ( void* )( new Int_t( t_original->GetNbinsZ() ) ) );
+    exported.push_back( ( void* )( new Double_t( t_original->GetZaxis()->GetXmin() ) ) );
+    exported.push_back( ( void* )( new Double_t( t_original->GetZaxis()->GetXmax() ) ) );
+
+    for( Int_t i = 1; i <= t_original->GetNbinsX(); ++i ) {
+        for( Int_t j = 1; j <= t_original->GetNbinsY(); ++j ) {
+            for( Int_t k = 1; k <= t_original->GetNbinsZ(); ++k ) {
+                exported.push_back( ( void* )( new Double_t( t_original->GetBinContent( i, j, k ) ) ) );
+                exported.push_back( ( void* )( new Double_t( t_original->GetBinError( i, j, k ) ) ) );
+            }
+        }
+    }
+
+    return exported;
+}
+
+TH3D* import_TH( vector< void* >& t_exported ) {
+    if( t_exported.empty() ) {
+        cout << "Error: Exported histogram is empty" << endl;
+        return nullptr;
+    }
+
+    TString*  name   = static_cast< TString* >( t_exported[ 0 ] );
+    TString*  title  = static_cast< TString* >( t_exported[ 1 ] );
+    Int_t*    nxbins = static_cast< Int_t* >( t_exported[ 2 ] );
+    Double_t* xlow   = static_cast< Double_t* >( t_exported[ 3 ] );
+    Double_t* xhigh  = static_cast< Double_t* >( t_exported[ 4 ] );
+    Int_t*    nybins = static_cast< Int_t* >( t_exported[ 5 ] );
+    Double_t* ylow   = static_cast< Double_t* >( t_exported[ 6 ] );
+    Double_t* yhigh  = static_cast< Double_t* >( t_exported[ 7 ] );
+    Int_t*    nzbins = static_cast< Int_t* >( t_exported[ 8 ] );
+    Double_t* zlow   = static_cast< Double_t* >( t_exported[ 9 ] );
+    Double_t* zhigh  = static_cast< Double_t* >( t_exported[ 10 ] );
+
+    TH3D* t_copy = new TH3D( *name, *title, *nxbins, *xlow, *xhigh, *nybins, *ylow, *yhigh, *nzbins, *zlow, *zhigh );
+
+    delete name;
+    delete title;
+    delete nxbins;
+    delete xlow;
+    delete xhigh;
+    delete nybins;
+    delete ylow;
+    delete yhigh;
+    delete nzbins;
+    delete zlow;
+    delete zhigh;
+
+    for( Int_t i = 1; i <= nxbins; ++i ) {
+        for( Int_t j = 1; j <= nybins; ++j ) {
+            for( Int_t k = 1; k <= nzbins; ++k ) {
+                Double_t* content = static_cast< Double_t* >(
+                    t_exported[ 11 + 2 * ( i - 1 ) * nybins * nzbins + 2 * ( j - 1 ) * nzbins + 2 * ( k - 1 ) ] );
+                Double_t* error = static_cast< Double_t* >(
+                    t_exported[ 12 + 2 * ( i - 1 ) * nybins * nzbins + 2 * ( j - 1 ) * nzbins + 2 * ( k - 1 ) ] );
+
+                t_copy->SetBinContent( i, j, k, *content );
+                t_copy->SetBinError( i, j, k, *error );
+
+                delete content;
+                delete error;
+            }
+        }
+    }
+
+    return t_copy;
+}
+
+template< typename type_ID, typename type_hist >
+THistMap< type_ID, type_hist >::THistMap() {
+}
+
+template< typename type_ID, typename type_hist >
+THistMap< type_ID, type_hist >::THistMap( const THistMap* t_THistMap ) {
+    for( pair< type_ID, type_hist* >& entry : *t_THistMap ) {
+        if( ! entry.second ) {
+            cout << "Error: Histogram is null" << endl;
+            continue;
+        }
+        type_hist* temp = copy_TH( entry.second );
+        if( ! temp ) {
+            cout << "Error: Could not copy histogram" << endl;
+            continue;
+        } else if( ! check_copy( entry.second, temp ) ) {
+            cout << "Error: Copied histogram does not match original" << endl;
+            delete temp;
+            continue;
+        }
+
+        auto result = this->insert( { entry.first, temp } );
+        if( ! result.second ) {
+            cout << "Error: Could not insert histogram into map" << endl;
+            delete temp;
+        }
+    }
+
+    if( this->size() != t_THistMap->size() ) {
+        cout << "Error: Not all histograms were copied" << endl;
+    }
+}
+
+template< typename type_ID, typename type_hist >
+THistMap< type_ID, type_hist >::THistMap( const THistMap& t_THistMap ) {
+    THistMap( &t_THistMap );
+}
+
+template< typename type_ID, typename type_hist >
+THistMap< type_ID, type_hist >::~THistMap() {
+    for( auto& entry : *this ) {
+        if( entry.second ) delete entry.second;
+    }
+}
+
+template< typename type_ID, typename type_hist >
+void THistMap< type_ID, type_hist >::operator=( const THistMap* t_THistMap ) {
+    THistMap( t_THistMap );
+}
+
+void THistMap< type_ID, type_hist >::operator=( const THistMap& t_THistMap ) {
+    THistMap( &t_THistMap );
+}
+
+template< typename type_ID, typename type_hist >
+THistReader< type_ID, type_hist >::THistReader( const vector< string >&  t_hists_paths,
+                                                const vector< type_ID >& t_hists_IDs,
+                                                const vector< string >&  t_hists_names ) {
+    if( t_hists_paths.size() != t_hists_IDs.size() || t_hists_paths.size() != t_hists_names.size() ) {
+        cout << "Error: hists_paths, hists_IDs, and hists_names "
+             << "vectors should all be the same length" << endl;
+        return;
+    }
+
+    for( int i{ 0 }; i < t_hists_paths.size(); i++ ) {
+        TFile file{ t_hists_paths[ i ].c_str() };
+        if( file.IsZombie() ) {
+            cout << "Error: Could not open file with path " << t_hists_paths[ i ] << endl;
+            continue;
+        }
+
+        pair< type_ID, type_hist* > entry;
+        entry.first = t_hists_IDs[ i ];
+
+        type_hist* temp{ nullptr };
+        file.GetObject( t_hists_names[ i ].c_str(), temp );
+        if( ! temp ) {
+            cout << "Error: Could not find histogram with name " << t_hists_names[ i ] << endl;
+            continue;
+        }
+
+        vector< void* > export { export_TH( temp ) };
+        if( export.empty() ) {
+            cout << "Error: Could not export histogram" << endl;
+            continue;
+        } else if( ! check_copy( temp, import_TH( export ) ) ) {
+            cout << "Error: Copied histogram does not match original" << endl;
+            continue;
+        }
+
+        file.Close();
+
+        entry.second = import_TH( export );
+        if( ! entry.second ) {
+            cout << "Error: Could not import histogram" << endl;
+            continue;
+        }
+
+        auto result = m_hists->insert( entry );
+        if( ! result.second ) {
+            cout << "Error: Could not insert histogram into map" << endl;
+        }
+    }
+
+    if( m_hists->size() != t_hists_paths.size() ) {
+        cout << "Error: Not all histograms were loaded" << endl;
+    }
+}
+
+template< typename type_ID, typename type_hist >
+THistReader< type_ID, type_hist >::THistReader( const THistReader* t_THistReader ) {
+    if( ! t_THistReader ) {
+        cout << "Error: THistReader is null" << endl;
+        return;
+    } else if ( ! t_THistReader->get_histsMap() ) {
+        cout << "Error: THistReader histogram map is null" << endl;
+        return;
+    }
+
+    m_hists = *( t_THistReader->get_histsMap() );
+
+    if( m_hists->size() != t_THistReader->get_histsMap()->size() ) {
+        cout << "Error: Not all histograms were copied" << endl;
+    }
+
+    return;
+}
+
+template< typename type_ID, typename type_hist >
+THistReader< type_ID, type_hist >::THistReader( const THistReader& t_THistReader ) {
+    THistReader( &t_THistReader );
+}
+
+template< typename type_ID, typename type_hist >
+THistReader< type_ID, type_hist >::~THistReader() {
+}
+
+template< typename type_ID, typename type_hist >
+type_hist* THistReader< type_ID, type_hist >::get_hist( const type_ID& t_ID ) const {
+    pair< type_ID, type_hist* >* result = m_hists->find( t_ID );
+    if( result == m_hists->end() ) {
+        cout << "Error: Could not find histogram with ID " << t_ID << endl;
+        return nullptr;
+    } else if ( ! result->second ) {
+        cout << "Error: Histogram is null" << endl;
+        return nullptr;
+    }
+
+    return result->second;
+}
+
+template< typename type_ID, typename type_hist >
+THistMap< type_ID, type_hist >* THistReader< type_ID, type_hist >::get_histsMap() const {
+    return new THistMap< type_ID, type_hist >( m_hists );
+}
+
+template< typename type_ID, typename type_hist >
+void THistReader< type_ID, type_hist >::operator=( const THistReader* t_THistReader ) {
+    THistReader( t_THistReader );
+}
+
+template< typename type_ID, typename type_hist >
+void THistReader< type_ID, type_hist >::operator=( const THistReader& t_THistReader ) {
+    THistReader( &t_THistReader );
+}
--- DataModel/THistReader.h
+++ DataModel/THistReader.h
@@ -4,7 +4,7 @@
 //*//                                                                                         //*//
 //*// Author:                                                                                 //*//
 //*//   Noah Everett (noah.everett@mines.sdsmt.edu)                                           //*//
-//*//   Created ¯\_(ツ)_/¯                                                                     //*//
+//*//   Created ¯\_(ツ)_/¯                                                                    //*//
 //*//                                                                                         //*//
 //*// About:                                                                                  //*//
 //*//   This file contains a general class for reading ROOT histograms.                       //*//
@@ -24,10 +24,8 @@
 #include "TFile.h"
 #include "TH1.h"
 #include "TH2.h"
-#include "TH3.h"
 
 #include <map>
-#include <memory>
 #include <ostream>
 #include <string>
 #include <vector>
@@ -35,163 +33,58 @@
 using std::cout;
 using std::endl;
 using std::map;
-using std::move;
+using std::pair;
 using std::string;
 using std::vector;
 
-class GlobalFileManager {
+TH1D* copy_TH( const TH1D* t_original );
+TH2D* copy_TH( const TH2D* t_original );
+TH3D* copy_TH( const TH3D* t_original );
+
+vector< void* > export_TH( const TH1D* t_original );
+vector< void* > export_TH( const TH2D* t_original );
+vector< void* > export_TH( const TH3D* t_original );
+
+import_TH( vector< void* >& t_exported, TH1D* t_copy );
+import_TH( vector< void* >& t_exported, TH2D* t_copy );
+import_TH( vector< void* >& t_exported, TH3D* t_copy );
+
+bool check_copy( const TH1D* t_original, TH1D* t_copy );
+bool check_copy( const TH2D* t_original, TH2D* t_copy );
+bool check_copy( const TH3D* t_original, TH3D* t_copy );
+
+template< typename type_ID, typename type_hist >
+class THistMap : map< type_ID, type_hist* > {
     public:
-        static GlobalFileManager& getInstance();
-        void                      addFile( TFile* file );
-        void                      addHistogram( void* hist );
-        ~GlobalFileManager();
+        THistMap();
+        THistMap( const THistMap* t_THistMap );
+        THistMap( const THistMap& t_THistMap );
 
-    private:
-        GlobalFileManager();
+        ~THistMap();
 
-        GlobalFileManager( const GlobalFileManager& )                  = delete;
-        GlobalFileManager&       operator=( const GlobalFileManager& ) = delete;
-
-        static GlobalFileManager m_instance;
-
-        vector< TFile* >         m_files;
-        vector< void* >          m_histograms;
+        void operator=( const THistMap* t_THistMap );
+        void operator=( const THistMap& t_THistMap );
 };
-
-inline GlobalFileManager::GlobalFileManager() {
-}
-
-inline GlobalFileManager& GlobalFileManager::getInstance() {
-    return m_instance;
-}
-
-inline void GlobalFileManager::addFile( TFile* file ) {
-    m_files.push_back( file );
-}
-
-inline void GlobalFileManager::addHistogram( void* hist ) {
-    m_histograms.push_back( hist );
-}
-
-GlobalFileManager::~GlobalFileManager() {
-    // Close all files
-    for( auto file : m_files ) {
-        if( file ) {
-            file->Close();
-            delete file;
-        }
-    }
-
-    // Delete all histograms
-    for( auto hist : m_histograms ) {
-        delete static_cast< TH1* >( hist );
-    }
-}
 
 template< typename type_ID, typename type_hist >
 class THistReader {
     public:
         THistReader( const vector< string >& t_hists_paths, const vector< type_ID >& t_hists_IDs,
                      const vector< string >& t_hists_names );
-        THistReader( const THistReader& other );
-        THistReader( THistReader&& other ) noexcept;
-        ~THistReader() = default;
-        type_hist*                        get_hist( const type_ID& t_ID ) const;
 
-        const map< type_ID, type_hist* >& get_histsMap() const {
-            return m_hists;
-        }
+        THistReader( const THistReader* t_THistReader );
+        THistReader( const THistReader& t_THistReader );
 
-        map< type_ID, type_hist* >* get_histsMap_cp() const;
-        THistReader&                operator=( const THistReader& other );
-        THistReader&                operator=( THistReader&& other ) noexcept;
+        ~THistReader();
+
+        type_hist*                      get_hist( const type_ID& t_ID ) const;
+        THistMap< type_ID, type_hist >* get_histsMap() const;
+
+        void operator=( const THistReader* t_THistReader );
+        void operator=( const THistReader& t_THistReader );
 
     private:
-        map< type_ID, type_hist* > m_hists;
+        THistMap< type_ID, type_hist > m_hists;
 };
-
-template< typename type_ID, typename type_hist >
-THistReader< type_ID, type_hist >::THistReader( const vector< string >&  t_hists_paths,
-                                                const vector< type_ID >& t_hists_IDs,
-                                                const vector< string >&  t_hists_names ) {
-    if( t_hists_paths.size() != t_hists_IDs.size() || t_hists_paths.size() != t_hists_names.size() ) {
-        cout << "Error: hists_paths, hists_IDs, and hists_names "
-             << "vectors should all be the same length" << endl;
-        return;
-    }
-
-    for( size_t i = 0; i < t_hists_paths.size(); i++ ) {
-        TFile* file = new TFile( t_hists_paths[ i ].c_str(), "READ" );
-        GlobalFileManager::getInstance().addFile( file );
-
-        if( file->IsZombie() ) {
-            cout << "Error: Could not open file with path " << t_hists_paths[ i ] << endl;
-            continue;
-        }
-
-        type_hist* hist = nullptr;
-        file->GetObject( t_hists_names[ i ].c_str(), hist );
-        if( ! hist ) {
-            cout << "Error: Could not find histogram with name " << t_hists_names[ i ] << endl;
-            continue;
-        }
-
-        m_hists[ t_hists_IDs[ i ] ] = hist;
-        GlobalFileManager::getInstance().addHistogram( hist );
-    }
-}
-
-template< typename type_ID, typename type_hist >
-THistReader< type_ID, type_hist >::THistReader( const THistReader& other )
-    : m_hists( other.m_hists ) {
-    for( const auto& pair : m_hists ) {
-        GlobalFileManager::getInstance().addHistogram( pair.second );
-    }
-}
-
-template< typename type_ID, typename type_hist >
-THistReader< type_ID, type_hist >::THistReader( THistReader&& other ) noexcept
-    : m_hists( move( other.m_hists ) ) {
-    for( const auto& pair : m_hists ) {
-        GlobalFileManager::getInstance().addHistogram( pair.second );
-    }
-}
-
-template< typename type_ID, typename type_hist >
-THistReader< type_ID, type_hist >& THistReader< type_ID, type_hist >::operator=( const THistReader& other ) {
-    if( this != &other ) {
-        m_hists = other.m_hists;
-        for( const auto& pair : m_hists ) {
-            GlobalFileManager::getInstance().addHistogram( pair.second );
-        }
-    }
-    return *this;
-}
-
-template< typename type_ID, typename type_hist >
-THistReader< type_ID, type_hist >& THistReader< type_ID, type_hist >::operator=( THistReader&& other ) noexcept {
-    if( this != &other ) {
-        m_hists = move( other.m_hists );
-        for( const auto& pair : m_hists ) {
-            GlobalFileManager::getInstance().addHistogram( pair.second );
-        }
-    }
-    return *this;
-}
-
-template< typename type_ID, typename type_hist >
-type_hist* THistReader< type_ID, type_hist >::get_hist( const type_ID& t_ID ) const {
-    auto it = m_hists.find( t_ID );
-    if( it != m_hists.end() ) {
-        return it->second;
-    }
-    cout << "Error: Histogram with ID " << t_ID << " not found." << endl;
-    return nullptr;
-}
-
-template< typename type_ID, typename type_hist >
-map< type_ID, type_hist* >* THistReader< type_ID, type_hist >::get_histsMap_cp() const {
-    return new map< type_ID, type_hist* >( m_hists );
-}
 
 #endif
