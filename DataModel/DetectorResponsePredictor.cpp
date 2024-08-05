0653c1ace2ccc9c8e26741d07264134c193cbad1
Noah Everett <noaheverett898@gmail.com>
2024-08-05 12:23:44 -0500

    Fix THistReader file close

--- DataModel/DetectorResponsePredictor.cpp
+++ DataModel/DetectorResponsePredictor.cpp
@@ -108,8 +108,8 @@ bool DetectorResponsePredictor::load_hists_emission(       shared_ptr< THistMap<
         return false;
     }
     // Delete t_hists_energies
+    cout << "t_hists_energies.use_count() = " << t_hists_energies.use_count() << endl;
     t_hists_energies.reset();
-    cout << "T_HISTS_ENERGIES.RESET()" << endl;
     
     // Load counts hist map
     histReader = new THistReader< int, TH2D >( t_hists_counts_paths, t_hists_IDs, t_hists_counts_names, t_hists_material+"_counts", m_verbosity_THistReader );
