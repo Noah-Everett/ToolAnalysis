e1f5a3f691dfd1e4c565d57d533ebf230cc1556d
Noah Everett <noaheverett898@gmail.com>
2024-08-05 15:50:55 -0500

    Fix THistReader file close

--- DataModel/THistReader.h
+++ DataModel/THistReader.h
@@ -770,10 +770,10 @@ THistMap< type_ID, type_hist >::~THistMap() {
     }
 
     // print all open root objects
-    auto list = gROOT->GetListOfCleanups();
-    cout << "THistMap: Number of open root objects: " << list->GetSize() << endl;
-    for( int i{ 0 }; i < list->GetSize(); i++ ) {
-        cout << "THistMap: Open root object: " << list->At( i )->GetName() << endl;
+    auto list = gROOT->GetRootFolder()->GetListOfFolders();
+    cout << "There are " << list->GetEntries() << " open root objects" << endl;
+    for( auto l = list->begin(); l != list->end(); ++l ) {
+        cout << "Object: " << ( *l )->GetName() << endl;
     }
 
     for( pair< const type_ID, type_hist* >& entry : *this ) {
