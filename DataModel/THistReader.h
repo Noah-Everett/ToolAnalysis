26c8e375312db81cb95eeee4a0590caeb261cbe1
Noah Everett <noaheverett898@gmail.com>
2024-08-05 12:37:47 -0500

    Fix THistReader file close

--- DataModel/THistReader.h
+++ DataModel/THistReader.h
@@ -768,7 +768,7 @@ THistMap< type_ID, type_hist >::~THistMap() {
         cout << "THistMap: Deleting histograms" << endl;
     }
 
-    for( pair< type_ID, type_hist* >& entry : *this ) {
+    for( pair< type_ID, type_hist* > entry : *this ) {
         if( entry.second ) {
             cout << "Deleting histogram with ID " << entry.first << endl;
             delete entry.second;
